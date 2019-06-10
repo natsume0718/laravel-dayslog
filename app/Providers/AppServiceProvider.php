e587093cecf2200cf6e91edda7b63ab54b67bbed
natsume0718 <moto718program@gmail.com>
2019-06-10 11:00:26 +0900

    [revert] バリデーションを削除して、twitterAPIでの投稿の可否で判断

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,16 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /**
-         * 全角半角を考慮したバリデーション
-         * 全角1 半角0.5
-         */
-        Validator::extend('full_harf_width_max', function ($attribute, $value, $parameters, $validator) {
-            $validator->addReplacer('full_harf_width_max', function ($message, $attribute, $rule, $parameters) {
-                return str_replace([':max'], $parameters, $message);
-            });
-            return (mb_strwidth($value) / 2) <= $parameters[0];
-        });
+
     }
 
     /**
